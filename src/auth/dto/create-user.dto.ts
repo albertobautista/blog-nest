1b047c3fde5347029a914dcdd700b4aa4855798d
Alberto Bautista <dachb101@gmail.com>
2024-01-19 21:20:21 -0600

    Add create and update user dtos

---
+++ src/auth/dto/create-user.dto.ts
@@ -0,0 +1,32 @@
+import {
+  IsBoolean,
+  IsEmail,
+  IsOptional,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from "class-validator";
+
+export class CreateUserDto {
+  @IsString()
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(5)
+  fullName: string;
+
+  @IsString()
+  @MinLength(6)
+  @MaxLength(15)
+  @Matches(/(?:(?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message:
+      "The password must have a Uppercase, lowercase letter and a number",
+  })
+  password: string;
+
+  @IsBoolean()
+  @IsOptional()
+  isAdmin: boolean;
+}
