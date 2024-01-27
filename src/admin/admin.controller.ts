1d6d7a62ec40aa9ce7f81bbeafa05fa79481cb29
Alberto Bautista <dachb101@gmail.com>
2024-01-26 19:13:14 -0600

    Use Auth deocrator in Admin module

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,21 +1,25 @@
 import { Controller, Delete, Get, Param, ParseUUIDPipe } from "@nestjs/common";
 import { AdminService } from "./admin.service";
+import { Auth } from "src/auth/dto/decorators/auth.decorator";
 
 @Controller("admin")
 export class AdminController {
   constructor(private readonly adminService: AdminService) {}
 
   @Get("users")
+  @Auth({ isAdmin: true })
   findAllUsers() {
     return this.adminService.findAllUsers();
   }
 
   @Delete("users/:id")
+  @Auth({ isAdmin: true })
   removeUser(@Param("id", ParseUUIDPipe) id: string) {
     return this.adminService.removeUser(id);
   }
 
   @Get("posts")
+  @Auth({ isAdmin: true })
   findAllPosts() {
     return this.adminService.findAllPosts();
   }
