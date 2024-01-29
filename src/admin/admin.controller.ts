92d0b95fcea61f0e9f466f30459efdbbc4c41977
Alberto Bautista <dachb101@gmail.com>
2024-01-28 19:27:24 -0600

    Add admin documentation

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,24 +1,31 @@
 import { Controller, Delete, Get, Param, ParseUUIDPipe } from "@nestjs/common";
 import { AdminService } from "./admin.service";
 import { Auth } from "src/auth/dto/decorators/auth.decorator";
-
+import { ApiBearerAuth, ApiOperation, ApiTags } from "@nestjs/swagger";
+@ApiTags("Admin")
 @Controller("admin")
 export class AdminController {
   constructor(private readonly adminService: AdminService) {}
 
   @Get("users")
+  @ApiBearerAuth()
+  @ApiOperation({ summary: "Get all users (protected)" })
   @Auth({ isAdmin: true })
   findAllUsers() {
     return this.adminService.findAllUsers();
   }
 
   @Delete("users/:id")
+  @ApiBearerAuth()
+  @ApiOperation({ summary: "Delete user by ID (protected)" })
   @Auth({ isAdmin: true })
   removeUser(@Param("id", ParseUUIDPipe) id: string) {
     return this.adminService.removeUser(id);
   }
 
   @Get("posts")
+  @ApiBearerAuth()
+  @ApiOperation({ summary: "Get all posts (protected)" })
   @Auth({ isAdmin: true })
   findAllPosts() {
     return this.adminService.findAllPosts();
