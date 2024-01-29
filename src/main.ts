705243c63f9bf8b17dcf45e904ccace24d2e97ee
Alberto Bautista <dachb101@gmail.com>
2024-01-28 19:22:58 -0600

    Set up swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { Logger, ValidationPipe } from "@nestjs/common";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,7 +13,18 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     })
   );
-  await app.listen(3000);
-  logger.log(`Application listening on port ${3000}`);
+
+  const config = new DocumentBuilder()
+    .setTitle("Blog RESTful API")
+    .setDescription("Blog endpoints")
+    .setVersion("1.0")
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup("api", app, document);
+
+  await app.listen(parseInt(process.env.PORT) || 3000);
+  logger.log(`Application listening on port ${process.env.PORT}`);
 }
 bootstrap();
